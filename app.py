a48d37ce6bda97e868446ed0f9ed3bc619a98251
John Lukach <john@lukach.net>
2026-03-29 13:12:08 +0000

    fix

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 
 import aws_cdk as cdk
 
-from webmonitor.webmonitor_database import WebmonitorDatabase
 from webmonitor.webmonitor_download import WebmonitorDownload
 from webmonitor.webmonitor_dynamodb import WebmonitorDynamoDB
 from webmonitor.webmonitor_github import WebmonitorGithub
@@ -13,17 +12,6 @@ from webmonitor.webmonitor_storage import WebmonitorStorage
 from webmonitor.webmonitor_ziplist import WebmonitorZiplist
 
 app = cdk.App()
-
-WebmonitorDatabase(
-    app, 'WebmonitorDatabase',
-    env = cdk.Environment(
-        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
-        region = 'us-east-2'
-    ),
-    synthesizer = cdk.DefaultStackSynthesizer(
-        qualifier = 'lukach'
-    )
-)
 
 WebmonitorDownload(
     app, 'WebmonitorDownload',
